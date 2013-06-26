2857b78d3e038439eed5ba8ed4b5ebb68cdc9f38
Jakub Turek jkbturek@gmail.com <jkbturek@gmail.com>
2013-06-27 01:01:50 +0200

    Added missing unit tests

--- CheatAR/development/server/word-dictionary-importer/src/test/java/pl/jakubturek/cheatar/server/wordimporter/tests/creation/WordFromStringCreatorTests.java
+++ CheatAR/development/server/word-dictionary-importer/src/test/java/pl/jakubturek/cheatar/server/wordimporter/tests/creation/WordFromStringCreatorTests.java
@@ -35,5 +35,31 @@ public class WordFromStringCreatorTests
 
             Assert.assertEquals("ademnss", word.getHash());
         }
+
+        @Test
+        public void setsCharacters()
+        {
+            WordFromStringCreator creator = new WordFromStringCreator();
+
+            Word word = creator.create("madness");
+            char[] characters = word.getCharacters().getCharacters();
+
+            Assert.assertEquals(15, characters.length);
+            Assert.assertEquals('m', characters[0]);
+            Assert.assertEquals('a', characters[1]);
+            Assert.assertEquals('d', characters[2]);
+            Assert.assertEquals('n', characters[3]);
+            Assert.assertEquals('e', characters[4]);
+            Assert.assertEquals('s', characters[5]);
+            Assert.assertEquals('s', characters[6]);
+            Assert.assertEquals('\0', characters[7]);
+            Assert.assertEquals('\0', characters[8]);
+            Assert.assertEquals('\0', characters[9]);
+            Assert.assertEquals('\0', characters[10]);
+            Assert.assertEquals('\0', characters[11]);
+            Assert.assertEquals('\0', characters[12]);
+            Assert.assertEquals('\0', characters[13]);
+            Assert.assertEquals('\0', characters[14]);
+        }
     }
 }
