3714ede4521000eb2dafd5b6e38e03ad3ed10fd0
mxf <maxfame@gmail.com>
2013-10-09 19:47:00 +0200

    Implemented basic dictionary statistic methods

--- Scrabble/creativity/word-statistics/calculator/statistics_calculator.py
+++ Scrabble/creativity/word-statistics/calculator/statistics_calculator.py
@@ -1,19 +1,37 @@
 
 class StatisticsCalculator(object):
 
+    __words = 0
+    __letters = 0
+    __letter_frequency = {}
+
     def __init__(self, words):
         self.__process_words(words)
 
     def __process_words(self, words):
-        pass
+        for word in words:
+            self.__increment_word_count()
+            self.__append_letter_probability_data(word)
+
+    def __increment_word_count(self):
+        self.__words += 1
+
+    def __append_letter_probability_data(self, word):
+        self.__letters += len(word)
+
+        for letter in word:
+            if letter not in self.__letter_frequency:
+                self.__letter_frequency[letter] = 0
+
+            self.__letter_frequency[letter] += 1
 
     def get_words_count(self):
-        pass
+        return self.__words
 
     def get_letter_probability(self, letter):
-        pass
+        letter_count = 0
 
-    def get_ngrams_sorted_by_frequency(self, ngram_size):
-        pass
+        if letter in self.__letter_frequency:
+            letter_count = self.__letter_frequency[letter]
 
-
+        return letter_count / self.__letters
