a3de3157e0386919799438d049bd278d76f47dcb
mxf <maxfame@gmail.com>
2013-10-09 20:12:26 +0200

    Introduced ngram dictionary statistic methods

--- Scrabble/creativity/word-statistics/calculator/statistics_calculator.py
+++ Scrabble/creativity/word-statistics/calculator/statistics_calculator.py
@@ -1,9 +1,12 @@
+from collections import OrderedDict
+
 
 class StatisticsCalculator(object):
 
     __words = 0
     __letters = 0
     __letter_frequency = {}
+    __ngrams = {}
 
     def __init__(self, words):
         self.__process_words(words)
@@ -12,6 +15,7 @@ class StatisticsCalculator(object):
         for word in words:
             self.__increment_word_count()
             self.__append_letter_probability_data(word)
+            self.__process_ngram_data(word)
 
     def __increment_word_count(self):
         self.__words += 1
@@ -25,6 +29,16 @@ class StatisticsCalculator(object):
 
             self.__letter_frequency[letter] += 1
 
+    def __process_ngram_data(self, word):
+        for ngram_length in range(1, len(word) + 1):
+            for ngram_offset in range(0, len(word) + 1 - ngram_length):
+                ngram = word[ngram_offset:ngram_offset + ngram_length]
+
+                if ngram not in self.__ngrams:
+                    self.__ngrams[ngram] = 0
+
+                self.__ngrams[ngram] += 1
+
     def get_words_count(self):
         return self.__words
 
@@ -35,3 +49,16 @@ class StatisticsCalculator(object):
             letter_count = self.__letter_frequency[letter]
 
         return letter_count / self.__letters
+
+    def get_ngram_count(self, ngram):
+        ngram_count = 0
+
+        if ngram in self.__ngrams:
+            ngram_count = self.__ngrams[ngram]
+
+        return ngram_count
+
+    def get_most_probable_ngrams(self, ngram_length):
+        filtered_ngrams = {x:self.__ngrams[x] for x in self.__ngrams if len(x) == ngram_length}
+
+        return OrderedDict(sorted(filtered_ngrams.items(), key=lambda x: filtered_ngrams[x]))
--- Scrabble/creativity/word-statistics/tests/statistics_calculator_tests.py
+++ Scrabble/creativity/word-statistics/tests/statistics_calculator_tests.py
@@ -22,3 +22,31 @@ class StatisticsCalculatorTests(TestCase):
         self.assertEqual(3/14, e_probability)
         self.assertEqual(2/14, o_probability)
         self.assertEqual(1/14, g_probability)
+
+    def test_ngram_count_calculation(self):
+        words = ['come', 'and', 'get', 'some', 'or', 'go', 'home']
+        calculator = StatisticsCalculator(words)
+
+        ome_count = calculator.get_ngram_count('ome')
+        e_count = calculator.get_ngram_count('e')
+        and_count = calculator.get_ngram_count('and')
+        rome_count = calculator.get_ngram_count('rome')
+
+        self.assertEqual(3, ome_count)
+        self.assertEqual(4, e_count)
+        self.assertEqual(1, and_count)
+        self.assertEqual(0, rome_count)
+
+    def test_most_probable_ngram_calculation(self):
+        words = ['come', 'and', 'get', 'some', 'random', 'or', 'go', 'home']
+        calculator = StatisticsCalculator(words)
+
+        most_probable_bigrams = calculator.get_most_probable_ngrams(2)
+        most_probable_trigrams = calculator.get_most_probable_ngrams(3)
+
+        self.assertEqual('om', most_probable_bigrams[0][0])
+        self.assertEqual(4, most_probable_bigrams[0][1])
+        self.assertEqual('ome', most_probable_trigrams[0][0])
+        self.assertEqual(3, most_probable_trigrams[0][1])
+        self.assertEqual('and', most_probable_trigrams[1][0])
+        self.assertEqual(2, most_probable_trigrams[1][1])
