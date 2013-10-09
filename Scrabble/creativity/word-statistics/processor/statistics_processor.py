4ff5eab9f7b52bedc568cb666b5a9edfac953dcc
mxf <maxfame@gmail.com>
2013-10-09 20:48:08 +0200

    Added polish dictionary to resources, introduced minimum interaction

--- Scrabble/creativity/word-statistics/processor/statistics_processor.py
+++ Scrabble/creativity/word-statistics/processor/statistics_processor.py
@@ -4,16 +4,24 @@ from exception.could_not_open_file import CouldNotOpenFile
 
 class StatisticsProcessor:
     calculator = StatisticsCalculator()
+    __read_words = 0
 
     def process_file(self, filename):
         try:
             with open(filename, 'r') as dictionary:
                 for word in dictionary:
+                    self.__handle_interaction()
                     self.calculator.process_word(word)
         except IOError:
             raise CouldNotOpenFile(filename)
 
         self.__print_summary()
 
+    def __handle_interaction(self):
+        self.__read_words += 1
+
+        if self.__read_words % 1000 == 0:
+            print "Processed " + str(self.__read_words) + " words..."
+
     def __print_summary(self):
-        print "summary"
+        print "Summary"
